c61bed8afa8c2dc1e9624fc0550f46043701be51
zengh17 <zengh17@deepseek.com>
2025-05-27 12:54:14 +0800

    PC试用成功

--- grokking/cli.py
+++ grokking/cli.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     parser.add_argument("--training_fraction", type=float, default=0.5)
     parser.add_argument("--prime", type=int, default=97)
     parser.add_argument("--num_layers", type=int, default=2)
-    parser.add_argument("--dim_model", type=int, default=128)
+    parser.add_argument("--dim_model", type=int, default=216) # 128
     parser.add_argument("--num_heads", type=int, default=4)
-    parser.add_argument("--batch_size", type=int, default=512)
-    parser.add_argument("--learning_rate", type=float, default=1e-3)
-    parser.add_argument("--weight_decay", type=float, default=1)
-    parser.add_argument("--num_steps", type=int, default=1e5)
+    parser.add_argument("--batch_size", type=int, default=512) #512
+    parser.add_argument("--learning_rate", type=float, default=1e-3) # 1e-3
+    parser.add_argument("--weight_decay", type=float, default=1) # 1
+    parser.add_argument("--num_steps", type=int, default=5e3) # 1e5
     parser.add_argument("--device", type=str, default="cpu")
     args = parser.parse_args()
 
